1aa35d02121bd79b5cdb77bcfcbc2e345535c5b4
hoanganh <nguyenhoanganh.hust62@gmail.com>
2020-06-26 21:54:34 -0700

    init

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-
-
-    
 });
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->middleware('verified');
